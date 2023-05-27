75b74dc35a2b89d3124224c211ccf3db0345a71c
pandamoon21 <33972938+pandamoon21@users.noreply.github.com>
2023-05-27 14:29:43 +0700

    Fix Credentials import module
    Signed-off-by: pandamoon21 <33972938+pandamoon21@users.noreply.github.com>

--- telegram_gcloner/utils/google_drive.py
+++ telegram_gcloner/utils/google_drive.py
@@ -7,6 +7,7 @@ import os
 from googleapiclient import errors
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 from google.oauth2 import service_account
 
 from utils.config_loader import config
@@ -31,7 +32,7 @@ class GoogleDrive:
         creds = None
         scopes = ['https://www.googleapis.com/auth/drive']
         if os.path.exists(token_file):
-            creds = service_account.Credentials.from_authorized_user_file(
+            creds = Credentials.from_authorized_user_file(
                 token_file, scopes=scopes)
         if os.path.exists(service_account_file):
             creds = service_account.Credentials.from_service_account_file(
